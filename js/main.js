993bb74331391a6b389f520c49c421e8aac66b27
Tenttu89 <91557666+Tenttu89@users.noreply.github.com>
2021-10-25 17:48:44 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-/* MAIN FUNCTIONS */
+/* FORM FUNCTIONS */
+
+const resetButton = document.getElementById('reset');
+const submitButton = document.getElementById('submit');
+
+const resetFunction = () => {
+    document.getElementById('firstName').innerHTML = '';
+    document.getElementById('lastName').innerHTML = '';
+    document.getElementById('phone').innerHTML = '';
+    document.getElementById('email').innerHTML = '';
+}
+
+const submitFunction = () => {
+    alert('This form is not really functional but your message should have been sent by clicking this button.');
+}
+
+resetButton.onclick = resetFunction;
+submitButton.onclick = submitFunction;
