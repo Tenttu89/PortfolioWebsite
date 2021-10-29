f5102ce59473555db16915fe4f7a294bc47a7f4a
Tenttu89 <91557666+Tenttu89@users.noreply.github.com>
2021-10-29 13:41:25 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -148,18 +148,97 @@ if (document.body.contains(document.getElementById('calcOuterContainer'))) {
         document.getElementById('calcOutputField2').value = outputArray.join(' ');
     }
 
+    const addButtonThreeToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnThree').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
+    const addButtonFourToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnFour').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
+    const addButtonFiveToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnFive').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
+    const addButtonSixToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnSix').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
+    const addButtonSevenToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnSeven').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
+    const addButtonEightToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnEight').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
+    const addButtonNineToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnNine').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
+    const addButtonZeroToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnZero').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
+    const addButtonDotToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnDot').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
     const addButtonPlusToArray = (event) => {
         outputArray.push(document.getElementById('calcBtnPlus').value);
         document.getElementById('calcOutputField2').value = outputArray.join(' ');
     }
 
+    const addButtonMinusToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnMinus').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
+    const addButtonDivisionToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnDivision').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
+    const addButtonMultiplyToArray = (event) => {
+        outputArray.push(document.getElementById('calcBtnMultiply').value);
+        document.getElementById('calcOutputField2').value = outputArray.join(' ');
+    }
+
+    const equationEquals = (event) => {
+        document.getElementById('calcOutputField1').value = outputArray.join(' ');
+        document.getElementById('calcOutputField2').value = 'Perkeleen resultti!!';
+    }
+
     const clearOutput = (event) => {
         document.getElementById('calcOutputField1').value = '';
         document.getElementById('calcOutputField2').value = '';
+        outputArray = [];
     }
 
     document.getElementById('calcBtnOne').onclick = addButtonOneToArray;
     document.getElementById('calcBtnTwo').onclick = addButtonTwoToArray;
+    document.getElementById('calcBtnThree').onclick = addButtonThreeToArray;
+    document.getElementById('calcBtnFour').onclick = addButtonFourToArray;
+    document.getElementById('calcBtnFive').onclick = addButtonFiveToArray;
+    document.getElementById('calcBtnSix').onclick = addButtonSixToArray;
+    document.getElementById('calcBtnSeven').onclick = addButtonSevenToArray;
+    document.getElementById('calcBtnEight').onclick = addButtonEightToArray;
+    document.getElementById('calcBtnNine').onclick = addButtonNineToArray;
+    document.getElementById('calcBtnZero').onclick = addButtonZeroToArray;
+    document.getElementById('calcBtnDot').onclick = addButtonDotToArray;
     document.getElementById('calcBtnPlus').onclick = addButtonPlusToArray;
+    document.getElementById('calcBtnMinus').onclick = addButtonMinusToArray;
+    document.getElementById('calcBtnDivision').onclick = addButtonDivisionToArray;
+    document.getElementById('calcBtnMultiply').onclick = addButtonMultiplyToArray;
+    document.getElementById('calcBtnEquals').onclick = equationEquals;
     document.getElementById('calcBtnClear').onclick = clearOutput;
 }
